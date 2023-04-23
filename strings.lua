f560a5ae1b8ed489ee83d7cfa43d00d4c583dc8c
Florian Thiel <flth@flth.de>
2023-04-23 16:13:13 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,50 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Tram T 27 / TM 33, TM 34, TM 36 (Berlin)",
+		["mod_desc"] = "The so-called \"Schützenwagen\" T 27 were intended for modernisation of the coach fleet, but suffered acute technical problems so that they all had to be withdrawn by 1931. A first conversion attempt resulted in the TM 33 series, which was introduced in 1933. These were designed for multiple control. After that, they tried to make the TM 34 cheaper and abandoned multiple control. For the 1936 Olympic Games, the rest of the fleet was modernised as the TM 36, this series being a mix of the previous ones. These were the last BVG (West) coaches in service until 1967.",
+
+        ["tm36_menu"] = "T 27 / TM 33 / 34 / 36 (Berlin)",
+        ["t27+t27_rolle_name"] = "T 27 Motor car + T 27 Motor car (1927)",
+		["t27_desc"] = "The T 27s were the so-called \"Schützenwagen\" and were intended to modernise transport in the German capital. Due to severe constructional safety deficiencies of the brake equipment, the complete series had to be taken out of service in 1931 (in the game they are available until 1933, until the successor is available). They had only one driver's cab per coach and were always coupled as twin railcars.",
+        ["tm33_rolle_name"] = "TM 33 Motor car (1933)",
+		["tm33+tm33_rolle_name"] = "TM 33 Motor car + TM 33 Motor car (1933)",
+        ["tm33_schere_name"] = "TM 33 Motor car (1948)",
+		["tm33+tm33_schere_name"] = "TM 33 Motor car + TM 33 Motor car (1948)",
+		["tm33_desc"] = "The TM 33 were the first conversions from the \"Schützenwagen\", which were parked due to technical problems. After the conversions, two railcars could be controlled with one driving switch, the current was transmitted by thick power cables to the end of the car.",
+        ["tm34_rolle_name"] = "TM 34 Motor car (1936)",
+        ["tm34_schere_name"] = "TM 34 Motor car (1948)",
+        ["tm34_desc"] = "The next TM 34 conversion series was cheaper and only had single control, so that the coaches could only be used individually or with sidecars.",
+		["tm36_rolle_name"] = "TM 36 Motor car (1936)",
+		["tm36+tm36_rolle_name"] = "TM 36 Motor car + TM 36 Motor car (1936)",
+        ["tm36_schere_name"] = "TM 36 Motor car (1948)",
+		["tm36+tm36_schere_name"] = "TM 36 Motor car + TM 36 Motor car (1948)",
+		["tm36_desc"] = "For the Olympic Games in 1936, modern trams were to be procured, so the legal four-axle cars were also converted to the TM 36 series. This was a symbiosis of TM 33 and TM 34 and again had compound control and the familiar heavy current cables. These were the last trams of the BVG (West) until 1967.",
+
+	},
+	de = {
+
+		["mod_name"] = "Tram T 27 / TM 33, TM 34, TM 36 (Berlin)",
+		["mod_desc"] = "Die sogenannten \"Schützenwagen\" T 27 waren zur Modernisierung des Wagenparks vorgesehen, litten aber akuten technischen Problemen, so dass sie bis 1931 alle abgestellt werden mussten. Ein erster Umbauversuch ergab die Reihe TM 33, die im Jahr 1933 vorgestellt wurde. Diese waren für Mehrfachsteuerung ausgelegt. Danach versuchte man die TM 34 günstiger zu gestalten und gab die Mehrfachsteuerung wieder auf. Zu den Olympischen Spielen 1936 modernisierte man dann die restliche Flotte als TM 36, wobei diese Baureihe ein Mix aus den vorangegangenen war. Bis 1967 waren dies die letzten im Einsatz befindlichen Wagen der BVG (West).",
+
+        ["tm36_menu"] = "T 27 / TM 33 / 34 / 36 (Berlin)",
+		["t27+t27_rolle_name"] = "T 27 Triebwagen + T 27 Triebwagen (1927)",
+		["t27_desc"] = "Die T 27 waren die sogenannten \"Schützenwagen\" und sollten den Verkehr in der deutschen Hauptstadt modernisieren. Wegen schwerer konstruktiver Sicherheitsmängel der Bremsausrüstung musste die komplette Serie 1931 aus dem Betrieb genommen werden (im Spiel sind sie bis 1933 verfügbar, bis der Nachfolger zur Verfügung steht). Sie besassen nur einen Führerstand pro Wagen und waren immer als Zwillingstriebwage gekuppelt.",
+        ["tm33_rolle_name"] = "TM 33 Triebwagen (1933)",
+		["tm33+tm33_rolle_name"] = "TM 33 Triebwagen + TM 33 Triebwagen (1933)",
+        ["tm33_schere_name"] = "TM 33 Triebwagen (1948)",
+		["tm33+tm33_schere_name"] = "TM 33 Triebwagen + TM 33 Triebwagen (1948)",
+		["tm33_desc"] = "Die TM 33 waren die ersten Umbauten aus den \"Schützenwagen\", die wegen technischer Probleme abgestellt waren. Nach den Umbauten konnten mit einem Fahrschalter zwei Triebwagen gesteuert werden, der Strom wurde durch dicke Starkstromkabel an den Wagenende übertragen.",
+        ["tm34_rolle_name"] = "TM 34 Triebwagen (1936)",
+        ["tm34_schere_name"] = "TM 34 Triebwagen (1948)",
+        ["tm34_desc"] = "Die nächste Umbauserie TM 34 wurde billiger ausgeführt nur mit Einfachsteuerung, so dass die Wagen nur einzeln oder mit Beiwagen eingesetzt werden konnten.",
+		["tm36_rolle_name"] = "TM 36 Triebwagen (1936)",
+		["tm36+tm36_rolle_name"] = "TM 36 Triebwagen + TM 36 Triebwagen (1936)",
+        ["tm36_schere_name"] = "TM 36 Triebwagen (1948)",
+		["tm36+tm36_schere_name"] = "TM 36 Triebwagen + TM 36 Triebwagen (1948)",
+		["tm36_desc"] = "Zu den Olympischen Spielen 1936 sollten moderne Straßenbahnen beschafft werden, also baute man die rechtlichen Vierachswagen auch um zur Serie TM 36. Diese war eine Symbiose aus TM 33 und TM 34 und hatte wieder Verbundsteuerung und die bekannten Starkstromkabel. Bis 1967 waren das die letzten Trams der BVG (West).",
+
+	}
+}
+end
